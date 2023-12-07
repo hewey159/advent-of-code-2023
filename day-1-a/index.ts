aa50e88eb4a7c23490e04ea6f6c8743ac3c7b777
Wade Hewett <wade.hewett@rea-group.com>
2023-12-07 19:20:09 +1030

    using NEA

--- day-1-a/index.ts
+++ day-1-a/index.ts
@@ -2,6 +2,7 @@ import { pipe } from "fp-ts/function";
 import * as A from "fp-ts/Array";
 import * as O from "fp-ts/Option";
 import * as fs from "fs";
+import * as NEA from "fp-ts/NonEmptyArray";
 
 const isNumber = (value: string) => {
   return !isNaN(parseInt(value));
@@ -9,22 +10,15 @@ const isNumber = (value: string) => {
 
 const stringToArray = (str: string): string[] => str.split("");
 
-const keepOnlyNumbers = (arr: string[]): O.Option<string[]> =>
-  pipe(
-    arr,
-    A.filter(isNumber),
-    O.fromPredicate((x) => x.length > 0) // if empty return none
-  );
+const keepOnlyNumbers = (arr: string[]): O.Option<NEA.NonEmptyArray<string>> =>
+  pipe(arr, A.filter(isNumber), NEA.fromArray);
 
 const getNumberFromWeirdString = (input_str: string) => {
   return pipe(
     input_str,
     stringToArray,
-    keepOnlyNumbers, //removes all non numbers
-    O.bindTo("input_arr"),
-    O.bind("left_value", ({ input_arr }) => A.head(input_arr)),
-    O.bind("right_value", ({ input_arr }) => A.last(input_arr)),
-    O.map(({ left_value, right_value }) => parseInt(left_value + right_value)),
+    keepOnlyNumbers,
+    O.map((input_arr) => parseInt(NEA.head(input_arr) + NEA.last(input_arr))),
     O.getOrElse(() => 0)
   );
 };
